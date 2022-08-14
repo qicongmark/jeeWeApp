022f291c5f8a6580e531e8c8d96d8357173e40e8
3289118 <328911897@qq.com>
2022-08-14 22:16:18 +0800

    qicong

--- miniprogram/pages/manage/manage.js
+++ miniprogram/pages/manage/manage.js
@@ -15,11 +15,18 @@ Page({
 
   },
 
+  //添加文章
+  addArticle: function(e){
+    wx.navigateTo({
+      url: '/pages/article/articleAdd/articleAdd',
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**

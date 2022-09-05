59106d258c763ae8709ee823747c318c802f32de
3289118 <328911897@qq.com>
2022-09-05 14:17:41 +0800

    qicong

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,7 +7,11 @@ const db = wx.cloud.database()
 Page({
 
   data: {
-    banners:[],
+    banners:[
+      {cover:"cloud://cloud1-9gpqtz2t48f59ac7.636c-cloud1-9gpqtz2t48f59ac7-1255805873/1661869755939.jpg"},
+      {cover:"cloud://cloud1-9gpqtz2t48f59ac7.636c-cloud1-9gpqtz2t48f59ac7-1255805873/1661868971320.jpg"},
+      {cover:"cloud://cloud1-9gpqtz2t48f59ac7.636c-cloud1-9gpqtz2t48f59ac7-1255805873/1661869669785.jpg"}
+    ],
     category:[
       {image:"source.png",name:"免费资源",code:1000,path:"/pages/srcs/srcs"},
       {image:"soft.png",name:"学习编程",code:1001,path:"/pages/srcs/srcs"},
